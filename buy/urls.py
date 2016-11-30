2e76d168ec70bd880a463ee3f4561a4afb7c3510
tylermcnish <tylermcnish@gmail.com>
2016-11-29 15:07:25 -1000

    update 11-29

--- buy/urls.py
+++ buy/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
                url(r'^design',views.design,name='design'),
+               url(r'^index',views.index,name='index'),
                url(r'^$',views.index,name='index')
                ]
