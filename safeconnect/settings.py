f1f9ed18c92e5199d17269ea18c29ec26beadcb0
tylermcnish <tylermcnish@gmail.com>
2016-12-13 08:50:03 -0800

    minor

--- safeconnect/settings.py
+++ safeconnect/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ianb4f58qje_tt-@=x4&j^762o)@vtzdkwn)2x73+7q%-4f^)i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1, '.pythonanywhere.com']
 
 
 # Application definition
